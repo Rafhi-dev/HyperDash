8766ca89ea21d11ef4366b5b753faea23fc30975
Rafhi-dev <rafhi.dev@gmail.com>
2025-07-20 10:36:05 +0700

    Add breadcrumb to BtnView component and adjust layout spacing

--- src/components/ui/button/BtnView.jsx
+++ src/components/ui/button/BtnView.jsx
@@ -1,31 +1,40 @@
-import BarChart from "../../charts/Bar/BarChart";
+import { faHome } from "@fortawesome/free-solid-svg-icons";
+import Breadcrumb from "../breadcrumb/Breadcrumb";
 import Card from "../card/Cards";
 import Btn from "./ButtonX";
 
 function BtnView() {
+  const setBreadCrumb = [
+    { label: "Home", url: "/", icon: faHome },
+    { label: "Component", url: "#" },
+    { label: "Button" },
+  ];
   return (
-    <div className="flex flex-col gap-4 p-4">
-      <Card w="full" title={"Button Solid"}>
-        <div className="space-x-4">
-          <Btn primary value="Primary" />
-          <Btn secondary value="Secondary" />
-          <Btn success value="Success" />
-          <Btn warning value="Warning" />
-          <Btn danger value="Danger" />
-          <Btn value="Default" />
-        </div>
-      </Card>
-      <Card w="full" title={"Button Outline"}>
-        <div className="space-x-4">
-          <Btn outline primary value="Primary" />
-          <Btn outline secondary value="Secondary" />
-          <Btn outline success value="Success" />
-          <Btn outline warning value="Warning" />
-          <Btn outline danger value="Danger" />
-          <Btn outline value="Default" className="unavailable" />
-        </div>
-      </Card>
-    </div>
+    <>
+      <Breadcrumb items={setBreadCrumb} className={"m-6"} />
+      <div className="flex flex-col m-6 gap-4">
+        <Card w="full" title={"Button Solid"}>
+          <div className="space-x-4">
+            <Btn primary value="Primary" />
+            <Btn secondary value="Secondary" />
+            <Btn success value="Success" />
+            <Btn warning value="Warning" />
+            <Btn danger value="Danger" />
+            <Btn value="Default" />
+          </div>
+        </Card>
+        <Card w="full" title={"Button Outline"}>
+          <div className="space-x-4">
+            <Btn outline primary value="Primary" />
+            <Btn outline secondary value="Secondary" />
+            <Btn outline success value="Success" />
+            <Btn outline warning value="Warning" />
+            <Btn outline danger value="Danger" />
+            <Btn outline value="Default" className="unavailable" />
+          </div>
+        </Card>
+      </div>
+    </>
   );
 }
 
